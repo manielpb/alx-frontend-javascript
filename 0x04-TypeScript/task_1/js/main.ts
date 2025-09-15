7864db6834c8fb94878470256be071530e8f8a02
maniel <manuelpepr99@icloud.com>
2025-09-15 21:08:47 +0000

    update

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,5 +17,5 @@ interface printTeacherFunction{
 }
 
 function printTeacher({firstName, lastName}: {firstName: string; lastName: string}): string {
-  return `${firstName[0]}. ${lastName}`;
+  return `${firstName}. ${lastName}`;
 }
